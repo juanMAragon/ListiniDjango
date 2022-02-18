10f5523c364633aba3898468fab4cafdb6107947
Juan Aragon <juanm.aramas@gmail.com>
2022-02-18 19:56:21 +0100

    cleanup

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kaumlg)e6+j*=0aqfsqn=g&1=rzl&36j5wp^zvsq#h4zyt%)l0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", '127.0.0.1', '0.0.0.0', '10.4.0.103']
 
 
 # Application definition
