c3c5ae0cef91023cc1cf8804bf903ae47632f701
Juan Aragon <juanm.aramas@gmail.com>
2022-02-18 13:02:03 +0100

    create list by forms

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import toDoList, Item
+from .forms import CreateNewList
 
 # Create your views here.
 def index(response, id):
     ls = toDoList.objects.get(id=id)
-    item = ls.item_set.get(id=1)
+    # item = ls.item_set.get(id=id)
     return render(response, "main/list.html", {"name":'list', "ls":ls})
 
 def home(response):
     return render(response, 'main/home.html', {"name":'home'})
+
+def create(response):
+
+    if response.method == 'POST':
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = toDoList(name=n)
+            t.save()
+        
+        return HttpResponseRedirect(f'/{t.id}')
+    else:
+        form = CreateNewList()
+        return render(response, 'main/create.html', {"name":"create", "form":form})
